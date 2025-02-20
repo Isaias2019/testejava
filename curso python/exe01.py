dc3f10f95b78efcab83cdfe94c9e2b931894b810
Isaias2019 <54869705+Isaias2019@users.noreply.github.com>
2025-02-20 03:50:38 +0000

    começando empython

---
+++ curso python/exe01.py
@@ -0,0 +1,7 @@
+print("CONHECENDO UM POUCO DE VOCE")
+
+nome = input('ola qual seu nome: ')
+ano = int(input('seu ano de nascimento com 4 digitos: '))
+anoAtual = 2025
+idadeAtual = anoAtual - ano
+print(f"ola meu amigo {nome} , e sua idade é {idadeAtual} Anos")
